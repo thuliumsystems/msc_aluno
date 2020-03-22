540aece69f2503b0dd04f88acdb6ec938bd5a583
thuliumsystems <comercial@thulium.com.br>
2020-03-22 10:24:05 -0400

    script para organizar a saída do perf

--- script.sh
+++ script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #remove as 3 primeiras linhas
-sed -e '1,3d' <arquivo.txt >sem_header.txt
+# sed -e '1,3d' <files/arquivo.txt >sem_header.txt
 
 #remove os espaços em branco e substitui por ;
-awk '{$1=$1}1' OFS=";" sem_header.txt >sem_espacos.txt
+sed -e '1,3d' <files/arquivo.txt | awk '{$1=$1}1' OFS=";" | cut -d ";" -f1,2 >files/filtrado.txt
 
 #filtra apenas as colunas tempo e valor
-cat sem_espacos.txt | cut -d ";" -f1,2 >filtrado.txt
+# cat files/sem_espacos.txt | cut -d ";" -f1,2 >files/filtrado.txt
 
 #cria o cabeçalho
-echo "time branch-instructions branch-misses bus-cycles cache-misses cache-references cpu-cycles instructions ref-cycles" >r.txt
+echo "time branch-instructions branch-misses bus-cycles cache-misses cache-references cpu-cycles instructions ref-cycles" >resultados/r.txt
 
 y=1
 while IFS=";" read -r time value; do
@@ -38,8 +38,8 @@ while IFS=";" read -r time value; do
   if [ $y -eq 8 ]; then
     r_8=$value
     #adiciona os valores linha por linha
-    echo $time $r_1 $r_2 $r_3 $r_4 $r_5 $r_6 $r_7 $r_8 >>r.txt
+    echo $time $r_1 $r_2 $r_3 $r_4 $r_5 $r_6 $r_7 $r_8 >>resultados/r.txt
     y=0
   fi
   let y=y+1
-done <filtrado.txt
+done <files/filtrado.txt
